1aab2eb019fa4be706746eb48b2fd98ad6ee342f
Sabino Ronaldo da Silva <jssrsilva49.dev@gmail.com>
2024-08-20 16:51:51 -0300

    Foi feito um ajuste no arquivo DesafioProjetoHospedagem.csproj, para melhorias na validação do código; geração de documentação e obrigatoriedade na utilização de declaração de uso de bibliotecas nas classes. Foram feitos ajustes nas classes Pessoa, Reserva e Suite, referentes a documentação dos métodos e propriedades; utilização de bibliotecas; adição de método para representar a classe como uma String. Foram criadas classes para gerenciamento de exceções e de extensão. A classe Program foi revisada, para melhor atender as requisições do projeto. Foi adicionada a biblioteca FAke.Net, para geração de dados aleatórios, para se terem testes mais aleatórios.

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -1,21 +1,59 @@
-namespace DesafioProjetoHospedagem.Models;
-
-public class Pessoa
+namespace DesafioProjetoHospedagem.Models
 {
-    public Pessoa() { }
+    using System;
+    using System.Globalization;
 
-    public Pessoa(string nome)
+    /// <summary>
+    /// Classe que representa uma pessoa
+    /// </summary>
+    /// <param name="nome">Nome da pessoa</param>
+    /// <param name="sobrenome">Sobrenome da pessoa</param>
+    /// <param name="idade">Idade da pessoa</param>
+    public class Pessoa(String nome, String sobrenome, Int32 idade)
     {
-        Nome = nome;
-    }
 
-    public Pessoa(string nome, string sobrenome)
-    {
-        Nome = nome;
-        Sobrenome = sobrenome;
-    }
+        /// <summary>
+        /// Método construtor da classe
+        /// </summary>
+        /// <param name="nome">Nome da pessoa</param>
+        public Pessoa(String nome) : this(nome, String.Empty, 0)
+        {
+        }
 
-    public string Nome { get; set; }
-    public string Sobrenome { get; set; }
-    public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+        /// <summary>
+        /// Método construtor da classe
+        /// </summary>
+        public Pessoa() : this(String.Empty, String.Empty, 0)
+        {
+        }
+
+        /// <summary>
+        /// Nome da pessoa
+        /// </summary>
+        public String Nome { get; set; } = nome;
+
+        /// <summary>
+        /// Sobrenome da pessoa
+        /// </summary>
+        public String Sobrenome { get; set; } = sobrenome;
+
+        /// <summary>
+        /// Nome completo da pessoa
+        /// </summary>
+        public String NomeCompleto => $"{Nome} {Sobrenome}".ToUpper(CultureInfo.CurrentCulture);
+
+        /// <summary>
+        /// Idade da pessoa
+        /// </summary>
+        public Int32 Idade { get; set; } = idade;
+
+        /// <summary>
+        /// Retorna uma representação da classe utilizando uma estrutura específica
+        /// </summary>
+        /// <returns>Uma String com os valores da classe interpolados</returns>
+        public override String ToString()
+        {
+            return $"Nome: {NomeCompleto} - idade: {Idade}";
+        }
+    }
 }
--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,60 +1,109 @@
 namespace DesafioProjetoHospedagem.Models
 {
-    public class Reserva
+    using DesafioProjetoHospedagem.Exceptions;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Classe que representa uma reserva feita por uma pessoa
+    /// </summary>
+    /// <param name="diasReservados">Quantidade de dias que a pessoa irá ficar hospedado.</param>
+    public class Reserva(Int32 diasReservados)
     {
-        public List<Pessoa> Hospedes { get; set; }
-        public Suite Suite { get; set; }
-        public int DiasReservados { get; set; }
 
-        public Reserva() { }
-
-        public Reserva(int diasReservados)
-        {
-            DiasReservados = diasReservados;
+        /// <summary>
+        /// Método construtor da classe
+        /// </summary>
+        public Reserva() : this(0)
+        { 
         }
 
-        public void CadastrarHospedes(List<Pessoa> hospedes)
+        /// <summary>
+        /// Quantidade de pessoas que irão utilizar a suíte do hotel
+        /// </summary>
+        public IEnumerable<Pessoa> Hospedes { get; private set; }
+
+        /// <summary>
+        /// Suite escolhida pela pessoa
+        /// </summary>
+        public Suite Suite { get; private set; }
+
+        /// <summary>
+        /// Quantidade de dias que a pessoa irá ficar hospedado.
+        /// </summary>
+        public Int32 DiasReservados { get; set; } = diasReservados;
+
+        /// <summary>
+        /// Método que cadastra os hóspedes
+        /// </summary>
+        /// <param name="hospedes">Hóspedes que serão cadastrados</param>
+        public void CadastrarHospedes(Pessoa[] hospedes)
         {
+            ArgumentNullException.ThrowIfNull(hospedes);
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade > (hospedes.Length-1))
             {
-                Hospedes = hospedes;
+                Hospedes = [.. hospedes];
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new CadastrarHospedesException($"A suite escolhida não tem capacidade suficiente para hospedar todos os hóspedes.\nCapacidade da suíte: {Suite.Capacidade}\nTotal de hóspedes: {hospedes.Length}");
             }
         }
 
+        /// <summary>
+        /// Cadastra a suíte escolhida pela pessoa
+        /// </summary>
+        /// <param name="suite">Suíte escolhida pela pessoa</param>
         public void CadastrarSuite(Suite suite)
         {
+            ArgumentNullException.ThrowIfNull(suite);
             Suite = suite;
         }
 
-        public int ObterQuantidadeHospedes()
+        /// <summary>
+        /// Retorna a quantidade de pessoas cadastradas
+        /// </summary>
+        /// <returns></returns>
+        public Int32 ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count();
         }
 
-        public decimal CalcularValorDiaria()
+        /// <summary>
+        /// Calcula o valor da diária
+        /// </summary>
+        /// <returns>Retorna o valor da diária</returns>
+        public Decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            Decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados > 9)
             {
-                valor = 0;
+                valor = valor - (valor * 0.10M);
             }
 
             return valor;
+        }
+
+        /// <summary>
+        /// Retorna uma representação da classe utilizando uma estrutura específica
+        /// </summary>
+        /// <returns>Uma String com os valores da classe interpolados</returns>
+        public override String ToString()
+        {
+            return $"Hóspedes: {ObterQuantidadeHospedes()}\nDias reservados: {DiasReservados}\nValor diária: {CalcularValorDiaria()}";
         }
     }
 }
